a0feae1063b72bb844391a5318d06cdb7261b9d5
rakshith <rakshithr1701@gmail.com>
2026-04-08 07:07:43 +0530

    basic image priocessing using cpp

---
+++ basics.cpp
@@ -0,0 +1,29 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main(){
+    std::string image_path = "data/img1.jpg";
+    cv::Mat image = cv::imread(image_path);
+
+    if (image.empty()){
+        std::cerr << "Error: Couldnot load image" << std::endl;
+        return -1;
+    }
+
+    std::cout << "Width:" << image.cols <<std::endl;
+    std::cout << "Height:" << image.rows <<std::endl;
+    std::cout << "Channels:" << image.channels() <<std::endl;
+
+    cv::Mat resized_image;
+    cv::resize(image, resized_image, cv::Size(400, 400));
+
+    cv::Mat gray_image;
+    cv::cvtColor(resized_image, gray_image, cv::COLOR_BGR2GRAY);
+
+    cv::imshow("Original Image", image);
+    cv::imshow("Resized Image", resized_image);
+    cv::imshow("Gray image", gray_image);
+
+    cv::waitKey(0);
+    return 0;
+}
