f90c8c9fc414fb5e91a4f553d42be096ab66c1e2
rakshith <rakshithr1701@gmail.com>
2026-04-09 07:02:47 +0530

    core image operations

--- core_image_ops.cpp
+++ core_image_ops.cpp
@@ -1,4 +1,31 @@
-#include <opencv2/opencv.hpp>
+#include "image_operations/image_operations.h"
 #include <iostream>
+// #include <opencv2/opencv.hpp>
 
-int main()
+
+int main(){
+    std::string image_path = "data/img1.jpg";
+
+    cv::Mat resize, gray, blurred, edge;
+
+
+    cv::Mat image = load_image(image_path);
+
+    if(image.empty()){
+        std::cerr << "Error: couldnot load image" << std::endl;
+        return -1;
+    }
+
+    cv::resize(image, image, cv::Size(400, 400));
+
+    cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);
+    cv::GaussianBlur(gray, blurred, cv::Size(5, 5), 0);
+    cv::Canny(blurred, edge, 100, 200);
+
+    display_image("Resized Image", resize);
+    display_image("Gray Image", gray);
+    display_image("Blurred Image", blurred);
+    display_image("Edge Image", edge);
+
+    return 0;
+}
