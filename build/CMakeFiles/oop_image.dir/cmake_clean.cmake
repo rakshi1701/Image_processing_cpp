edc13695f3466330d0d800ddad12314e581ea0fe
rakshith <rakshithr1701@gmail.com>
2026-04-14 06:22:43 +0530

    structurised

---
+++ build/CMakeFiles/oop_image.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oop_image.dir/src/image_operations/basic_oop.cpp.o"
+  "CMakeFiles/oop_image.dir/src/image_operations/basic_oop.cpp.o.d"
+  "CMakeFiles/oop_image.dir/src/image_operations/image_operations.cpp.o"
+  "CMakeFiles/oop_image.dir/src/image_operations/image_operations.cpp.o.d"
+  "oop_image"
+  "oop_image.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oop_image.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/oop_image.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for oop_image.
+# This may be replaced when dependencies are built.
